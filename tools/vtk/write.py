d493142fa31fdea56ce26c1713782784bd18241e
Weicheng Pei <weicheng.pei@icloud.com>
2019-08-31 16:02:32 +0800

    增加网格复杂度

--- tools/vtk/write.py
+++ tools/vtk/write.py
@@ -1,65 +1,59 @@
 import vtk
 
-# 3 ----- 2 ----- 5
-# | (1) / |       |
-# |   /   |  (0)  |
-# | / (2) |       |
-# 0 ----- 1 ----- 4
-xyz =((0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0), (2, 0, 0), (2, 1, 0))
+# 创建「非结构网格 (vtkUnstructuredGrid)」
+# vtkDataSet <- vtkPointSet <- vtkUnstructuredGridBase <- vtkUnstructuredGrid
+grid = vtk.vtkUnstructuredGrid()
 
-# 创建 vtkPoints (点集) 对象
+# 7 ------------- 6
+# |               |
+# |      (3)      |
+# |               |
+# 3 ----- 2 ----- 5
+# | (0) / |       |
+# |   /   |  (2)  |
+# | / (1) |       |
+# 0 ----- 1 ----- 4
+xyz =((0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0),
+      (2, 0, 0), (2, 1, 0), (2, 2, 0), (0, 2, 0))
+
+# 创建「点集 (vtkPoints)」
 points = vtk.vtkPoints()
 points.SetNumberOfPoints(len(xyz))
-# 创建 vtkFloatArray (float 型数组) 对象
+# 创建「数组 (vtkFloatArray)」
 vector_on_points = vtk.vtkFloatArray()
-# virtual void vtkAbstractArray::SetName(const char* name)	
+# virtual void vtkAbstractArray::SetName(const char*)	
 vector_on_points.SetName("Position")
 vector_on_points.SetNumberOfComponents(3)
 vector_on_points.SetNumberOfTuples(len(xyz))
-for i in range(6):
+for i in range(len(xyz)):
   points.InsertPoint(i, xyz[i])
   vector_on_points.InsertTuple(i, xyz[i])
-
-# 创建 vtkUnstructuredGrid (非结构网格) 对象
-grid = vtk.vtkUnstructuredGrid()
+# virtual void vtkPointSet::SetPoints(vtkPoints*)
 grid.SetPoints(points)
 # vtkPointData* vtkDataSet::GetPointData()
 data_on_points = grid.GetPointData()
-# int vtkDataSetAttributes::SetVectors(vtkDataArray* da)
+# int vtkDataSetAttributes::SetVectors(vtkDataArray*)
 data_on_points.SetVectors(vector_on_points)
 
-# void vtkUnstructuredGrid::Allocate(vtkIdType numCells=1000, int extSize=1000)
-# extSize 现已弃用, 不需要设置
-grid.Allocate(3)
-
-# 创建 vtkQuad (四边形单元) 对象
-cell = vtk.vtkQuad()
-# 定义 局部结点编号 到 全局结点编号 的映射
-# vtkIdList* vtkCell::GetPointIds ()
-id_list = cell.GetPointIds()
-# void vtkIdList::SetId(const vtkIdType i, const vtkIdType vtkid)
-id_list.SetId(0, 1)
-id_list.SetId(1, 4)
-id_list.SetId(2, 5)
-id_list.SetId(3, 2)
-# 将单元添加进 grid 中
-# vtkIdType vtkUnstructuredGridBase::InsertNextCell(int type, vtkIdList* ptIds)
-grid.InsertNextCell(cell.GetCellType(), id_list)
-
-# 创建第 1 个 vtkTriangle (三角形单元) 对象, 并添加进 grid 中
-cell = vtk.vtkTriangle()
-id_list = cell.GetPointIds()
-id_list.SetId(0, 0)
-id_list.SetId(1, 2)
-id_list.SetId(2, 3)
-grid.InsertNextCell(cell.GetCellType(), id_list)
-# 创建第 2 个 vtkTriangle (三角形单元) 对象, 并添加进 grid 中
-cell = vtk.vtkTriangle()
-id_list = cell.GetPointIds()
-id_list.SetId(0, 2)
-id_list.SetId(1, 1)
-id_list.SetId(2, 0)
-grid.InsertNextCell(cell.GetCellType(), id_list)
+# void vtkUnstructuredGrid::Allocate(vtkIdType numCells=1000)
+grid.Allocate(4)
+# vtkIdList* vtkCell::GetPointIds()
+# vtkIdList::SetId(const vtkIdType i_local, const vtkIdType i_global)
+# vtkUnstructuredGridBase::InsertNextCell(int type, vtkIdList* id_list)
+# 添加两个「三角形单元 (vtkTriangle)」
+for global_id_list in ((0, 2, 3), (2, 1, 0)):
+  cell = vtk.vtkTriangle()
+  id_list = cell.GetPointIds()
+  for i in range(3):
+    id_list.SetId(i, global_id_list[i])
+  grid.InsertNextCell(cell.GetCellType(), id_list)
+# 添加两个「四边形单元 (vtkQuad)」
+for global_id_list in ((1, 4, 5, 2), (5, 6, 7, 3)):
+  cell = vtk.vtkQuad()
+  id_list = cell.GetPointIds()
+  for i in range(4):
+    id_list.SetId(i, global_id_list[i])
+  grid.InsertNextCell(cell.GetCellType(), id_list)
 
 # 按 传统 VTK 格式 输出
 writer = vtk.vtkDataSetWriter()
