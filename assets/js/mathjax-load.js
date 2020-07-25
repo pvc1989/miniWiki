1d8b06493b80d5e3dff51075784cac6b218143b2
Weicheng Pei <weicheng.pei@icloud.com>
2020-07-26 01:48:52 +0800

    用 MathJax 替换 KaTeX

---
+++ assets/js/mathjax-load.js
@@ -0,0 +1,22 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+  },
+};
+(function () {
+  function stripCDATA(x) {
+    if (x.startsWith('% <![CDATA[') && x.endsWith('%]]>'))
+      return x.substring(11,x.length-4);
+    return x;
+  }
+  document.querySelectorAll("script[type='math/tex']").forEach(function(el) {
+    el.outerHTML = "\\(" + stripCDATA(el.textContent) + "\\)";
+  });
+  document.querySelectorAll("script[type='math/tex; mode=display']").forEach(function(el) {
+    el.outerHTML = "\\[" + stripCDATA(el.textContent) + "\\]";
+  });
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
