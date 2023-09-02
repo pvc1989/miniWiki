51bd15b4d644b7503eb89b35f81846bfb49e6302
Weicheng Pei <weicheng.pei@icloud.com>
2023-09-02 14:31:10 +0800

    Remove mid-level families.

--- programming/mesh/cgns/add_family.py
+++ programming/mesh/cgns/add_family.py
@@ -32,8 +32,12 @@ def getChildrenByType(node, type):
 
 def getUniqueChildByType(node, type):
     children = getChildrenByType(node, type)
-    assert 1 == len(children)
-    return children[0]
+    if 1 == len(children):
+        return children[0]
+    elif 0 == len(children):
+        return None
+    else:
+        assert False
 
 
 def getNodeName(node) -> str:
@@ -83,9 +87,54 @@ def rename(old_file):
     cgm.save(new_file, tree)
 
 
+def getFamilyToRoot(families) -> dict:
+    family_to_root = dict()
+    for family in families:
+        children = getChildrenByType(family, 'FamilyName_t')
+        if 1 == len(children):
+            child = children[0]
+            family_to_root[getNodeName(family)] = cgu.getValueAsString(child)
+        elif 0 == len(children):
+            name = getNodeName(family)
+            family_to_root[name] = name
+        else:
+            assert False
+    print(family_to_root)
+    return family_to_root
+
+
+def compress(old_file):
+    tree, links, paths = cgm.load(old_file)
+    base = getUniqueChildByType(tree, 'CGNSBase_t')
+    families = getChildrenByType(base, 'Family_t')
+    family_to_root = getFamilyToRoot(families)
+    for family_name, root_name in family_to_root.items():
+        if family_name == root_name:
+            print('Keep (Family_t)', family_name)
+        else:
+            print('Remove (Family_t)', family_name)
+            cgu.removeChildByName(base, family_name)
+    zones = getChildrenByType(base, 'Zone_t')
+    for zone in zones:
+        bc = getUniqueChildByType(zone, 'ZoneBC_t')
+        bc_list = getChildrenByType(bc, 'BC_t')
+        for bc in bc_list:
+            bc_name = getNodeName(bc)
+            print(f'In (BC_t) {bc_name}:')
+            family = getUniqueChildByType(bc, 'FamilyName_t')
+            family_name = cgu.getValueAsString(family)
+            root_name = family_to_root[family_name]
+            print(f'  Change (FamilyName_t) {family_name} to {root_name}')
+            cgu.setValue(family, cgu.setStringAsArray(root_name))
+    new_file = 'family_compressed.cgns'
+    cgm.save(new_file, tree)
+    return new_file
+
+
 if __name__ == '__main__':
     old = sys.argv[1]
     copied = copy(old)
     read(copied)
     added = add(copied, ['Fluid', 'Wall', 'Left', 'Right'])
     renamed = rename(added)
+    compressed = compress(added)
