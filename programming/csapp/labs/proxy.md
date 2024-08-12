9e1d4a5ad10184e0164c0ae8afffa1238464ef22
Weicheng <weicheng.pei@icloud.com>
2024-08-12 20:37:56 +0800

    Initialize by mocking the iterative echo server.

--- programming/csapp/labs/proxy.md
+++ programming/csapp/labs/proxy.md
@@ -17,6 +17,10 @@ make
 
 # 1. Implementing a sequential web proxy
 
+## 1.0 Initialize the Server
+
+Just mock [`echoserveri.c`](../code/netp/echoserveri.c) and [`echo.c`](../code/netp/echo.c).
+
 # 2. Dealing with multiple concurrent requests
 
 # 3. Caching web objects
--- programming/csapp/labs/proxy/proxy.c
+++ programming/csapp/labs/proxy/proxy.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include "csapp.h"
+
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
@@ -7,8 +9,38 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
+void serve(int connfd) {
+    size_t n; 
+    char buf[MAXLINE]; 
+    rio_t rio;
+
+    Rio_readinitb(&rio, connfd);
+    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
+        printf("%s", buf);
+    }
+}
+
+int main(int argc, char **argv)
 {
-    printf("%s", user_agent_hdr);
-    return 0;
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;  /* Enough space for any address */
+    char client_hostname[MAXLINE], client_port[MAXLINE];
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(0);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(struct sockaddr_storage); 
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        Getnameinfo((SA *) &clientaddr, clientlen, client_hostname, MAXLINE, 
+                    client_port, MAXLINE, 0);
+        printf("Connected to (%s, %s)\n", client_hostname, client_port);
+        serve(connfd);
+        Close(connfd);
+    }
+    exit(0);
 }
