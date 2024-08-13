cffe6da26b9f4634c2eb0fe05d2ab15b25dab929
Weicheng <weicheng.pei@icloud.com>
2024-08-13 10:13:13 +0800

    Support reading from and writing to the client.

--- programming/csapp/labs/proxy/proxy.c
+++ programming/csapp/labs/proxy/proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 #include "csapp.h"
 
@@ -9,15 +10,84 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
+int read_one_line(rio_t *rio, char *line) {
+    int n = Rio_readlineb(rio, line, MAXLINE);
+    assert(n >= 2);
+    assert(line[n - 2] == '\r');
+    assert(line[n - 1] == '\n');
+    printf("< %s", line);
+    return n;
+}
+
+/**
+ * @brief Returns an error message to the client.
+ * 
+ */
+void clienterror(int fd, char *cause, char *errnum, 
+                 char *shortmsg, char *longmsg)
+{
+    char buf[MAXLINE];
+
+    /* Print the HTTP response headers */
+    sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+
+    /* Print the HTTP response body */
+    sprintf(buf, "<html><title>Tiny Proxy Error</title>");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<body bgcolor=""ffffff"">\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "%s: %s\r\n", errnum, shortmsg);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<p>%s: %s\r\n", longmsg, cause);
+    Rio_writen(fd, buf, strlen(buf));
+    sprintf(buf, "<hr><em>The Tiny Proxy server</em>\r\n");
+    Rio_writen(fd, buf, strlen(buf));
+}
+
 void serve(int connfd) {
-    size_t n; 
-    char buf[MAXLINE]; 
+    // Read the entire HTTP request from the client and check whether the it is valid.
+    size_t n = 0;
+    char buf[MAXLINE], *line = buf;
     rio_t rio;
 
     Rio_readinitb(&rio, connfd);
-    while((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0) {
-        printf("%s", buf);
+    // Read the first line:
+    char method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    n = read_one_line(&rio, line);
+    sscanf(line, "%s %s %s", method, uri, version);
+    if (strcasecmp(method, "GET")) {
+        clienterror(connfd, method, "501", "Not Implemented",
+                    "Tiny Proxy does not implement this method");
+        return;
     }
+    // Read other lines:
+    do {
+      line += n;  // n does not account '\0'
+      n = read_one_line(&rio, line);
+    } while (n != 2);
+    assert(strcmp(line, "\r\n") == 0);
+    assert(strlen(buf) <= MAXLINE);
+    printf("Length of the request: %ld\n", strlen(buf));
+    // Connect to the appropriate web server.
+
+    // Request the object the client specified.
+
+    // Read the server's response and forward it to the client.
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    printf("> %s", buf);
+    Rio_writen(connfd, buf, strlen(buf));
+    sprintf(buf, "Server: Tiny Proxy Server\r\n");
+    printf("> %s", buf);
+    Rio_writen(connfd, buf, strlen(buf));
+    sprintf(buf, "Connection: close\r\n");
+    printf("> %s", buf);
+    Rio_writen(connfd, buf, strlen(buf));
+    sprintf(buf, "\r\n");
+    printf("> %s", buf);
+    Rio_writen(connfd, buf, strlen(buf));
 }
 
 int main(int argc, char **argv)
