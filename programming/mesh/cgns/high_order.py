b7384b0f8c395a0960e83d4ea34c42f5c090e8e5
Weicheng <weicheng.pei@icloud.com>
2024-10-17 09:31:33 +0800

    Shift added points to triangles.

--- programming/mesh/cgns/high_order.py
+++ programming/mesh/cgns/high_order.py
@@ -9,6 +9,29 @@ import argparse
 import wrapper
 
 
+def getFootOnTriangle(point: np.ndarray, kdtree: KDTree, connectivity: np.ndarray,
+        x: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
+    distance_to_center, i_cell = kdtree.query(point)
+    first = i_cell * 3
+    a, b, c = connectivity[first : first + 3] - 1
+    point_a = np.array([x[a], y[a], z[a]])
+    point_b = np.array([x[b], y[b], z[b]])
+    point_c = np.array([x[c], y[c], z[c]])
+    normal = np.cross(point_b - point_a, point_c - point_a)
+    normal /= np.linalg.norm(normal)  # (A, B, C)
+    assert (np.linalg.norm(normal) - 1) < 1e-6, np.linalg.norm(normal)
+    offset = -normal.dot(point_a)  # D
+    assert abs(normal.dot(point_a) + offset) < 1e-6, normal.dot(point_a) + offset
+    assert abs(normal.dot(point_b) + offset) < 1e-6, normal.dot(point_b) + offset
+    assert abs(normal.dot(point_c) + offset) < 1e-6, normal.dot(point_c) + offset
+    ratio = -normal.dot(point) - offset
+    foot = point + ratio * normal
+    assert abs(normal.dot(foot) + offset) < 1e-6, normal.dot(foot) + offset
+    distance_to_triangle = np.linalg.norm(foot - point)
+    assert distance_to_triangle <= distance_to_center, (distance_to_triangle, distance_to_center)
+    return foot
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog = 'python3 high_order.py')
@@ -38,17 +61,24 @@ if __name__ == "__main__":
         wrapper.getUniqueChildByType(cad_zone, 'GridCoordinates_t'), 'DataArray_t')
     cad_coords_x, cad_coords_y, cad_coords_z = cad_coords[X][1], cad_coords[Y][1], cad_coords[Z][1]
 
+    cad_connectivity = np.ndarray((n_cell * 3,), dtype='int')
     sections = wrapper.getChildrenByType(cad_zone, 'Elements_t')
     centers = np.ndarray((n_cell, 3))
     i_cell_global = 0
     for section in sections:
-        # if args.verbose:
-        #     print(section)
+        if args.verbose and False:
+            print(section)
         element_type = wrapper.getNodeData(section)
         assert element_type[0] == 5  # TRI_3
         # build DS for quick nearest neighbor query
         connectivity = wrapper.getNodeData(
             wrapper.getUniqueChildByName(section, 'ElementConnectivity'))
+        element_range = wrapper.getNodeData(wrapper.getUniqueChildByName(section, 'ElementRange'))
+        first = element_range[0] - 1
+        last = element_range[1]  # inclusive to exclusive
+        if args.verbose:
+            print('element_range =', element_range, first, last)
+        cad_connectivity[first * 3 : last * 3] = connectivity
         for i_cell_local in range(connectivity.shape[0] // 3):
             first = i_cell_local * 3
             index = connectivity[first : first + 3] - 1
@@ -98,36 +128,40 @@ if __name__ == "__main__":
         x = (mesh_coords_x[old_first + 0] + mesh_coords_x[old_first + 1]) / 2
         y = (mesh_coords_y[old_first + 0] + mesh_coords_y[old_first + 1]) / 2
         z = (mesh_coords_z[old_first + 0] + mesh_coords_z[old_first + 1]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
+        foot = getFootOnTriangle(np.array([x, y, z]), cad_kdtree, cad_connectivity, cad_coords_x, cad_coords_y, cad_coords_z)
+        new_coords_x[i_node_next] = foot[X]
+        new_coords_y[i_node_next] = foot[Y]
+        new_coords_z[i_node_next] = foot[Z]
         i_node_next += 1
         new_connectivity[new_fisrt + 4] = i_node_next
         # build the mid-point on the right edge
         x = (mesh_coords_x[old_first + 1] + mesh_coords_x[old_first + 2]) / 2
         y = (mesh_coords_y[old_first + 1] + mesh_coords_y[old_first + 2]) / 2
         z = (mesh_coords_z[old_first + 1] + mesh_coords_z[old_first + 2]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
+        foot = getFootOnTriangle(np.array([x, y, z]), cad_kdtree, cad_connectivity, cad_coords_x, cad_coords_y, cad_coords_z)
+        new_coords_x[i_node_next] = foot[X]
+        new_coords_y[i_node_next] = foot[Y]
+        new_coords_z[i_node_next] = foot[Z]
         i_node_next += 1
         new_connectivity[new_fisrt + 5] = i_node_next
         # build the mid-point on the top edge
         x = (mesh_coords_x[old_first + 2] + mesh_coords_x[old_first + 3]) / 2
         y = (mesh_coords_y[old_first + 2] + mesh_coords_y[old_first + 3]) / 2
         z = (mesh_coords_z[old_first + 2] + mesh_coords_z[old_first + 3]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
+        foot = getFootOnTriangle(np.array([x, y, z]), cad_kdtree, cad_connectivity, cad_coords_x, cad_coords_y, cad_coords_z)
+        new_coords_x[i_node_next] = foot[X]
+        new_coords_y[i_node_next] = foot[Y]
+        new_coords_z[i_node_next] = foot[Z]
         i_node_next += 1
         new_connectivity[new_fisrt + 6] = i_node_next
         # build the mid-point on the left edge
         x = (mesh_coords_x[old_first + 3] + mesh_coords_x[old_first + 0]) / 2
         y = (mesh_coords_y[old_first + 3] + mesh_coords_y[old_first + 0]) / 2
         z = (mesh_coords_z[old_first + 3] + mesh_coords_z[old_first + 0]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
+        foot = getFootOnTriangle(np.array([x, y, z]), cad_kdtree, cad_connectivity, cad_coords_x, cad_coords_y, cad_coords_z)
+        new_coords_x[i_node_next] = foot[X]
+        new_coords_y[i_node_next] = foot[Y]
+        new_coords_z[i_node_next] = foot[Z]
         i_node_next += 1
         new_connectivity[new_fisrt + 7] = i_node_next
         # build the center point
@@ -135,11 +169,14 @@ if __name__ == "__main__":
         x = np.sum(mesh_coords_x[index]) / 4
         y = np.sum(mesh_coords_y[index]) / 4
         z = np.sum(mesh_coords_z[index]) / 4
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
+        foot = getFootOnTriangle(np.array([x, y, z]), cad_kdtree, cad_connectivity, cad_coords_x, cad_coords_y, cad_coords_z)
+        new_coords_x[i_node_next] = foot[X]
+        new_coords_y[i_node_next] = foot[Y]
+        new_coords_z[i_node_next] = foot[Z]
         i_node_next += 1
         new_connectivity[new_fisrt + 8] = i_node_next
+        if args.verbose:
+            print(f'QUAD_4[{i_cell_mesh}] -> QUAD_9[{i_cell_mesh}]')
     assert i_node_next == n_node_new
     if args.verbose:
         print(f'in quadratic mesh: n_node = {n_node_new}, n_cell = {n_cell}')
