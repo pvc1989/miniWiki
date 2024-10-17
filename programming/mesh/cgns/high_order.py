72395cebbd3829da4736e91d3f7854b1b56f66ef
Weicheng <weicheng.pei@icloud.com>
2024-10-17 15:43:57 +0800

    Fix the `first_global` vs `first_local` bug.

--- programming/mesh/cgns/high_order.py
+++ programming/mesh/cgns/high_order.py
@@ -9,26 +9,42 @@ import argparse
 import wrapper
 
 
-def getFootOnTriangle(point: np.ndarray, kdtree: KDTree, connectivity: np.ndarray,
+def getFootOnTriangle(point_p: np.ndarray, kdtree: KDTree, connectivity: np.ndarray,
         x: np.ndarray, y: np.ndarray, z: np.ndarray) -> np.ndarray:
-    distance_to_center, i_cell = kdtree.query(point)
+    # return point_p
+    distance_to_center, i_cell = kdtree.query(point_p)
     first = i_cell * 3
-    a, b, c = connectivity[first : first + 3] - 1
+    index = connectivity[first : first + 3] - 1
+    a, b, c = index
     point_a = np.array([x[a], y[a], z[a]])
     point_b = np.array([x[b], y[b], z[b]])
     point_c = np.array([x[c], y[c], z[c]])
     normal = np.cross(point_b - point_a, point_c - point_a)
     normal /= np.linalg.norm(normal)  # (A, B, C)
-    assert (np.linalg.norm(normal) - 1) < 1e-6, np.linalg.norm(normal)
+    # assert (np.linalg.norm(normal) - 1) < 1e-6, np.linalg.norm(normal)
     offset = -normal.dot(point_a)  # D
-    assert abs(normal.dot(point_a) + offset) < 1e-6, normal.dot(point_a) + offset
-    assert abs(normal.dot(point_b) + offset) < 1e-6, normal.dot(point_b) + offset
-    assert abs(normal.dot(point_c) + offset) < 1e-6, normal.dot(point_c) + offset
-    ratio = -normal.dot(point) - offset
-    foot = point + ratio * normal
-    assert abs(normal.dot(foot) + offset) < 1e-6, normal.dot(foot) + offset
-    distance_to_triangle = np.linalg.norm(foot - point)
-    assert distance_to_triangle <= distance_to_center, (distance_to_triangle, distance_to_center)
+    # assert abs(normal.dot(point_a) + offset) < 1e-6, normal.dot(point_a) + offset
+    # assert abs(normal.dot(point_b) + offset) < 1e-6, normal.dot(point_b) + offset
+    # assert abs(normal.dot(point_c) + offset) < 1e-6, normal.dot(point_c) + offset
+    ratio = -normal.dot(point_p) - offset
+    foot = point_p + ratio * normal
+    # assert abs(normal.dot(foot) + offset) < 1e-6, normal.dot(foot) + offset
+    # distance_to_triangle = np.linalg.norm(foot - point_p)
+    # print('p =', point_p)
+    # print('a =', point_a, np.linalg.norm(point_p - point_a))
+    # print('b =', point_b, np.linalg.norm(point_p - point_b))
+    # print('c =', point_c, np.linalg.norm(point_p - point_c))
+    # center = (point_a + point_b + point_c) / 3
+    # if not (center == kdtree.data[i_cell]).all():
+    #     print('i_cell =', i_cell)
+    #     print('center =', center)
+    #     print('kdtree.data[i] =', kdtree.data[i_cell])
+    #     print(np.linalg.norm(center - kdtree.data[i_cell]))
+    #     assert False
+    # assert distance_to_triangle <= np.linalg.norm(point_p - point_a)
+    # assert distance_to_triangle <= np.linalg.norm(point_p - point_b)
+    # assert distance_to_triangle <= np.linalg.norm(point_p - point_c)
+    # assert distance_to_triangle <= distance_to_center, (distance_to_triangle, distance_to_center)
     return foot
 
 
@@ -61,33 +77,60 @@ if __name__ == "__main__":
         wrapper.getUniqueChildByType(cad_zone, 'GridCoordinates_t'), 'DataArray_t')
     cad_coords_x, cad_coords_y, cad_coords_z = cad_coords[X][1], cad_coords[Y][1], cad_coords[Z][1]
 
-    cad_connectivity = np.ndarray((n_cell * 3,), dtype='int')
+    cad_connectivity = np.zeros(n_cell * 3, dtype=int) - 1
+    # assert cad_connectivity.shape == (n_cell * 3,)
+    # assert (cad_connectivity == -1).all()
     sections = wrapper.getChildrenByType(cad_zone, 'Elements_t')
-    centers = np.ndarray((n_cell, 3))
+    print('n_section =', len(sections))
+    centers = np.zeros((n_cell, 3), dtype=float)
     i_cell_global = 0
     for section in sections:
         if args.verbose and False:
             print(section)
         element_type = wrapper.getNodeData(section)
-        assert element_type[0] == 5  # TRI_3
+        # assert element_type[0] == 5  # TRI_3
         # build DS for quick nearest neighbor query
         connectivity = wrapper.getNodeData(
             wrapper.getUniqueChildByName(section, 'ElementConnectivity'))
         element_range = wrapper.getNodeData(wrapper.getUniqueChildByName(section, 'ElementRange'))
-        first = element_range[0] - 1
-        last = element_range[1]  # inclusive to exclusive
+        first_global = element_range[0] - 1
+        last_global = element_range[1]  # inclusive to exclusive
         if args.verbose:
-            print('element_range =', element_range, first, last)
-        cad_connectivity[first * 3 : last * 3] = connectivity
-        for i_cell_local in range(connectivity.shape[0] // 3):
-            first = i_cell_local * 3
-            index = connectivity[first : first + 3] - 1
-            centers[i_cell_global][X] = np.sum(cad_coords_x[index]) / 3
-            centers[i_cell_global][Y] = np.sum(cad_coords_y[index]) / 3
-            centers[i_cell_global][Z] = np.sum(cad_coords_z[index]) / 3
+            print('element_range =', element_range, first_global, last_global)
+        cad_connectivity[first_global * 3 : last_global * 3] = connectivity[:]
+        n_cell_local = last_global - first_global
+        # assert n_cell_local == connectivity.shape[0] // 3
+        for i_cell_local in range(n_cell_local):
+            first_local = i_cell_local * 3
+            index = connectivity[first_local : first_local + 3] - 1
+            # assert (0 <= index).all() and (index < n_node).all()
+            # a, b, c = index
+            # centers[i_cell_global][X] = (cad_coords_x[a] + cad_coords_x[b] + cad_coords_x[c]) / 3            
+            # centers[i_cell_global][Y] = (cad_coords_y[a] + cad_coords_y[b] + cad_coords_y[c]) / 3
+            # centers[i_cell_global][Z] = (cad_coords_z[a] + cad_coords_z[b] + cad_coords_z[c]) / 3
+            curr_global = first_global + i_cell_local
+            centers[curr_global][X] = np.sum(cad_coords_x[index]) / 3
+            centers[curr_global][Y] = np.sum(cad_coords_y[index]) / 3
+            centers[curr_global][Z] = np.sum(cad_coords_z[index]) / 3
             i_cell_global += 1
-    assert i_cell_global == n_cell, i_cell_global
+    # assert i_cell_global == n_cell, i_cell_global
+    # assert (1 <= cad_connectivity).all() and (cad_connectivity <= n_node).all()
+
+    section_names = []
+    for section in sections:
+        section_names.append(wrapper.getNodeName(section))
+    # assert len(section_names) == len(sections)
+    for name in section_names:
+        cgu.removeChildByName(cad_zone, name)
+    # assert len(wrapper.getChildrenByType(cad_zone, 'Elements_t')) == 0
+    cgl.newElements(cad_zone, 'SingleSection', etype='TRI_3', erange=np.array([1, n_cell]), econnectivity=cad_connectivity)
+    print(len(wrapper.getChildrenByType(cad_zone, 'Elements_t')))
+    cgm.save('cad-merged.cgns', cad_cgns)
+
     cad_kdtree = KDTree(centers)
+    for i in range(n_cell):
+        pass
+        # assert np.linalg.norm(centers[i] - cad_kdtree.data[i]) == 0
 
     # load the linear mesh
     mesh_cgns, _, _ = cgm.load(args.mesh)
@@ -106,8 +149,8 @@ if __name__ == "__main__":
 
     section = wrapper.getUniqueChildByType(mesh_zone, 'Elements_t')
     element_type = wrapper.getNodeData(section)
-    assert element_type[0] == 7  # QUAD_4
-    connectivity = wrapper.getNodeData(
+    # assert element_type[0] == 7  # QUAD_4
+    old_connectivity = wrapper.getNodeData(
         wrapper.getUniqueChildByName(section, 'ElementConnectivity'))
 
     # add center and mid-edge points:
@@ -119,11 +162,12 @@ if __name__ == "__main__":
     new_coords_z = np.ndarray((n_node_new,), dtype=mesh_coords_z.dtype)
     new_coords_z[0 : n_node] = mesh_coords_z
     i_node_next = n_node
-    new_connectivity = np.ndarray((connectivity.shape[0] * 9 // 4,), dtype=connectivity.dtype)
+    new_connectivity = np.ndarray((old_connectivity.shape[0] * 9 // 4,),
+        dtype=old_connectivity.dtype)
     for i_cell_mesh in range(n_cell):
         old_first = i_cell_mesh * 4
         new_fisrt = i_cell_mesh * 9
-        new_connectivity[new_fisrt : new_fisrt + 4] = connectivity[old_first : old_first + 4]
+        new_connectivity[new_fisrt : new_fisrt + 4] = old_connectivity[old_first : old_first + 4]
         # build the mid-point on the bottom edge
         x = (mesh_coords_x[old_first + 0] + mesh_coords_x[old_first + 1]) / 2
         y = (mesh_coords_y[old_first + 0] + mesh_coords_y[old_first + 1]) / 2
@@ -165,7 +209,7 @@ if __name__ == "__main__":
         i_node_next += 1
         new_connectivity[new_fisrt + 7] = i_node_next
         # build the center point
-        index = connectivity[old_first : old_first + 4] - 1
+        index = old_connectivity[old_first : old_first + 4] - 1
         x = np.sum(mesh_coords_x[index]) / 4
         y = np.sum(mesh_coords_y[index]) / 4
         z = np.sum(mesh_coords_z[index]) / 4
@@ -175,9 +219,8 @@ if __name__ == "__main__":
         new_coords_z[i_node_next] = foot[Z]
         i_node_next += 1
         new_connectivity[new_fisrt + 8] = i_node_next
-        if args.verbose:
-            print(f'QUAD_4[{i_cell_mesh}] -> QUAD_9[{i_cell_mesh}]')
-    assert i_node_next == n_node_new
+        print(f'QUAD_4[{i_cell_mesh}] -> QUAD_9[{i_cell_mesh}]')
+    # assert i_node_next == n_node_new
     if args.verbose:
         print(f'in quadratic mesh: n_node = {n_node_new}, n_cell = {n_cell}')
 
