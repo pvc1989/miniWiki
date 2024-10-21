00c7cd147442f137528ce27ad638e6a9a11f972b
Weicheng <weicheng.pei@icloud.com>
2024-10-21 17:48:59 +0800

    Support parallel version of projection-to-CAD.

--- programming/mesh/cgns/project_to_cad.py
+++ programming/mesh/cgns/project_to_cad.py
@@ -3,6 +3,7 @@ from OCC.Core.gp import gp_Pnt, gp_Dir, gp_Lin
 from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeVertex
 from OCC.Core.BRepExtrema import BRepExtrema_DistShapeShape
 from OCC.Core.TopoDS import TopoDS_Shape, TopoDS_Vertex
+from check_nodes import getUniqueZone, readPoints
 
 import numpy as np
 import argparse
@@ -10,8 +11,61 @@ import sys
 from mpi4py import MPI
 
 
-def project_points(cad_file: str, inch_csv: np.ndarray, comm_rank: int, comm_size: int):
-    log = open(f'./occt/log_{comm_rank}.txt', 'w')
+def multiple_to_unique(multiple_points: np.ndarray) -> np.ndarray:
+    old_coords = multiple_points
+
+    print(old_coords[0, :])
+    print(old_coords[-1, :])
+
+    n_old = len(old_coords)
+
+    coord_to_i_new = dict()
+
+    i_new = 0
+    for xyz in old_coords:
+        x, y, z = xyz
+        coord = (x, y, z)
+        if coord not in coord_to_i_new:
+            coord_to_i_new[coord] = i_new
+            i_new += 1
+    assert i_new == len(coord_to_i_new)
+    n_new = i_new
+
+    i_old_to_i_new = np.ndarray((n_old, 1), dtype='int')
+    for i_old in range(n_old):
+        xyz = old_coords[i_old]
+        coord = (xyz[0], xyz[1], xyz[2])
+        i_old_to_i_new[i_old] = coord_to_i_new[coord]
+    np.savetxt('i_old_to_i_new.csv', i_old_to_i_new, fmt='%d', delimiter=',')
+
+    new_coords = np.ndarray((n_new, 3), old_coords.dtype)
+    for coord, i_new in coord_to_i_new.items():
+        new_coords[i_new] = coord
+
+    unique_points = new_coords
+    np.savetxt('unique_points.csv', unique_points, delimiter=',')
+    print(n_new, n_old)
+    return unique_points
+
+
+def unique_to_multiple(unique_points: np.ndarray) -> np.ndarray:
+    new_coords = unique_points
+    i_old_to_i_new = np.loadtxt('i_old_to_i_new.csv', dtype=int, delimiter=',')
+    n_old = len(i_old_to_i_new)
+
+    # new_coords = np.loadtxt('shifted_unique_coords.csv', delimiter=',')
+    old_coords = np.ndarray((n_old, 3), dtype=new_coords.dtype)
+
+    for i_old in range(n_old):
+        i_new = i_old_to_i_new[i_old]
+        old_coords[i_old] = new_coords[i_new]
+
+    multiple_points = old_coords
+    np.savetxt('multiple_points.csv', multiple_points, delimiter=',')
+
+
+def project_points(cad_file: str, inch_csv: str, comm_rank: int, comm_size: int) -> np.ndarray:
+    log = open(f'log_{comm_rank}.txt', 'w')
 
     log.write(f'reading CAD file on rank {comm_rank}\n')
     step_reader = STEPControl_Reader()
@@ -33,7 +87,7 @@ def project_points(cad_file: str, inch_csv: np.ndarray, comm_rank: int, comm_siz
     last = n_node_local * (comm_rank + 1)
     last = min(last, n_node_global)
 
-    new_coords = np.ndarray((n_node_local), old_coords.dtype)
+    new_coords = np.ndarray((n_node_local, 3), old_coords.dtype)
 
     log.write(f'range[{comm_rank}] = [{first}, {last})\n')
     for i in range(first, last):
@@ -47,8 +101,9 @@ def project_points(cad_file: str, inch_csv: np.ndarray, comm_rank: int, comm_siz
 
     new_coords /= 25.4  # now, new_coords in inch
 
-    np.savetxt(f'projected_coords_{comm_rank}.csv', new_coords, delimiter=',')
+    np.savetxt(f'new_coords_{comm_rank}.csv', new_coords, delimiter=',')
     log.close()
+    return new_coords
 
 
 if __name__ == "__main__":
@@ -56,7 +111,7 @@ if __name__ == "__main__":
         prog = f'python3 {sys.argv[0]}.py',
         description = 'Project points in CGNS file to the surface of a CAD model.')
     parser.add_argument('--cad', type=str, help='the STEP file of the CAD model')
-    parser.add_argument('--points', type=str, help='the CSV file of the points to be projected')
+    parser.add_argument('--mesh', type=str, help='the CGNS file of the points to be projected')
     parser.add_argument('--output', type=str, help='the CSV file of the projected points')
     parser.add_argument('--verbose', default=False, action='store_true')
     args = parser.parse_args()
@@ -69,4 +124,10 @@ if __name__ == "__main__":
     size = comm.Get_size()
     print(f"Hello from process {rank} of {size}!")
 
-    project_points(args.cad, args.points, rank, size)
+    if rank == 0:
+        cgns, zone, zone_size = getUniqueZone(args.mesh)
+        _, xyz, x, y, z = readPoints(zone, zone_size)
+        unique_xyz = multiple_to_unique(xyz)
+    comm.Barrier()
+
+    shifted_unique_xyz = project_points(args.cad, 'unique_points.csv', rank, size)
