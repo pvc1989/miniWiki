c856235448b87506f681a5ed59ed5e1236308d64
Weicheng <weicheng.pei@icloud.com>
2024-11-13 14:55:12 +0800

    Write log and npy files in a temporary folder.

--- programming/mesh/cgns/shift_by_rbf.py
+++ programming/mesh/cgns/shift_by_rbf.py
@@ -2,9 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy import sparse
 from scipy.spatial import KDTree
+import os
 import sys
+import shutil
 import argparse
-from timeit import default_timer as timer
+import time
 from concurrent.futures import ProcessPoolExecutor, wait
 
 import CGNS.MAP as cgm
@@ -80,11 +82,14 @@ def get_neighbors(kdtree: KDTree, point_i: np.ndarray, k_neighbor: int, radius: 
     return j_neighbors, radius
 
 
-def build_dok_matrix(kdtree: KDTree, k_neighbor: int, radius: float, verbose: bool) -> sparse.dok_matrix:
+def build_dok_matrix(kdtree: KDTree, args: str) -> sparse.dok_matrix:
     points = kdtree.data
     n_point = len(points)
     assert points.shape[1] == 3
     dok_matrix = sparse.dok_matrix((n_point + 4, n_point + 4))
+    k_neighbor = args.k_neighbor
+    radius = args.radius
+    verbose = args.verbose
     for i_point in range(n_point):
         point_i = points[i_point]
         dok_matrix[i_point, n_point] = dok_matrix[n_point, i_point] = 1
@@ -100,93 +105,56 @@ def build_dok_matrix(kdtree: KDTree, k_neighbor: int, radius: float, verbose: bo
                 print(f'i = {i_point:2d}, j = {j_point:2d}, d_ij = {distance_ij:.2e}')
             dok_matrix[i_point, j_point] = dimensional_rbf(distance_ij, radius_out)
         if verbose:
-            print(f'point[{i_point}] has {len(j_neighbors)} neighbors')
+            print(f'point[{i_point} / {n_point}] has {len(j_neighbors)} neighbors')
     if verbose and n_point <= 100:
         print('points =\n', points)
         print('matrix =\n', dok_matrix.todense())
     return dok_matrix
 
-
-def test_on_random_points(n_point: int, radius: float, verbose: bool):
-    np.random.seed(123456789)
-    points = np.random.rand(n_point, 3)
-    if verbose:
-        print('building the KDTree ...')
-    kdtree = KDTree(points)
-    if verbose:
-        print('building the dok_matrix ...')
-    dok_matrix = build_dok_matrix(kdtree, 0, radius, verbose)
-    sparsity = dok_matrix.nnz / (n_point * n_point)
-    print(f'nnz / (n * n) = {dok_matrix.nnz} / {n_point * n_point} = {sparsity:.2e}')
-    csc_matrix = dok_matrix.tocsc()
-    csc_file_name = f'csc_matrix_{n_point}.npz'
-    sparse.save_npz(csc_file_name, csc_matrix)
-    csc_matrix = sparse.load_npz(csc_file_name)
-    u_bc = np.random.rand(n_point + 4)
-    u_bc[-4:] = 0
-    u_coeffs, exit_code = sparse.linalg.lgmres(csc_matrix, u_bc, maxiter=20000, rtol=1e-7)
-    print('exit_code =', exit_code)
-    print(np.linalg.norm(csc_matrix.dot(u_coeffs) - u_bc))
-    print(np.allclose(csc_matrix.dot(u_coeffs), u_bc))
-    den_matrix = dok_matrix.todense()
-    u_coeffs = np.linalg.solve(den_matrix, u_bc)
-    print(np.linalg.norm(den_matrix.dot(u_coeffs) - u_bc))
-    print(np.allclose(den_matrix.dot(u_coeffs), u_bc))
-
-
-def build_rbf_matrix(old_points: np.ndarray, k_neighbor: int, radius: float, verbose: bool) -> str:
+def build_rbf_matrix(rbf_folder: str, old_points: np.ndarray) -> str:
+    start = time.time()
     assert old_points.shape[1] == 3
-    print('shape =', old_points.shape)
     n_point = len(old_points)
-    if verbose:
-        print('building the KDTree ...')
-        print(old_points)
+    print('building the KDTree ...')
     kdtree = KDTree(old_points)
-    if verbose:
-        print('building the dok_matrix ...')
-    dok_matrix = build_dok_matrix(kdtree, k_neighbor, radius, verbose)
+    print('building the dok_matrix ...')
+    dok_matrix = build_dok_matrix(kdtree, args)
     sparsity = dok_matrix.nnz / (n_point * n_point)
     print(f'nnz / (n * n) = {dok_matrix.nnz} / {n_point * n_point} = {sparsity:.2e}')
     csc_matrix = dok_matrix.tocsc()
-    if k_neighbor > 0:
-        assert radius == 0.0
-        csc_file_name = f'csc_matrix_n={n_point}_k={k_neighbor}.npz'
-    elif radius > 0:
-        assert k_neighbor == 0
-        csc_file_name = f'csc_matrix_n={n_point}_r={radius:.2e}.npz'
+    csc_file_name = f'{rbf_folder}/rbf_matrix.npz'
     sparse.save_npz(csc_file_name, csc_matrix)
-    return csc_file_name
+    end = time.time()
+    print(f'building RBF matrix costs {end - start} seconds')
 
 
-def solve_rbf_system(rbf_matrix_npz: str, rhs_columns: np.ndarray, verbose: bool) -> str:
-    rbf_matrix = sparse.load_npz(rbf_matrix_npz)
+def solve_rbf_system(rbf_folder: str, rhs_columns: np.ndarray) -> str:
+    rbf_matrix = sparse.load_npz(f'{rbf_folder}/rbf_matrix.npz')
     n_point = len(rhs_columns)
     n_column = rhs_columns.shape[1]
     sol_columns = np.ndarray((n_point + 4, n_column))
     for i_column in range(n_column):
         column_i = np.zeros((n_point + 4,))
         column_i[0 : n_point] = rhs_columns[:, i_column]
-        if verbose:
-            print(f'\nsolving column[{i_column}] ...')
-        start = timer()
+        print(f'\nsolving column[{i_column}] ...')
+        start = time.time()
         sol_columns[:, i_column], exit_code = sparse.linalg.lgmres(
             rbf_matrix, column_i, maxiter=20000, atol=0.0, rtol=1e-7)
         print(f'column[{i_column}] solved with exit_code = {exit_code}')
-        end = timer()
+        end = time.time()
         print(f'costs {end - start:.2e} seconds')
-    sol_file_name = f'solved_{rbf_matrix_npz[:-4]}.npy'
+    sol_file_name = f'{rbf_folder}/rbf_solutions.npy'
     np.save(sol_file_name, sol_columns)
-    return sol_file_name
 
 
-def shift_points_by_futures_process(i_task: int, n_task: int, k_neighbor: int, radius: float):
+def shift_points_by_futures_process(i_task: int, n_task: int, temp_folder: str, args: str):
     global kdtree
     global u_rbf, v_rbf, w_rbf
     global global_x, global_y, global_z
 
-    log = open(f'log_{i_task}.txt', 'w')
+    log = open(f'{temp_folder}/log_{i_task}.txt', 'w')
 
-    start = timer()
+    start = time.time()
 
     n_global = len(global_x)
     n_local = (n_global + n_task - 1) // n_task
@@ -197,8 +165,9 @@ def shift_points_by_futures_process(i_task: int, n_task: int, k_neighbor: int, r
         n_local = last - first
         assert last == n_global
     log.write(f'range[{i_task}] = [{first}, {last})\n')
-    log.flush()
 
+    k_neighbor = args.k_neighbor
+    radius = args.radius
     shift = np.zeros((n_local, 3), global_x.dtype)
     for i_local in range(n_local):
         i_global = i_local + first
@@ -218,52 +187,45 @@ def shift_points_by_futures_process(i_task: int, n_task: int, k_neighbor: int, r
             w += w_rbf[j_global] * phi_ij
         shift[i_local, :] = u, v, w
         log.write(f'{i_global} shifted by ({u:.2e}, {v:.2e}, {w:.2e})\n')
-        log.flush()
-    end = timer()
+    end = time.time()
     log.write(f'wall-clock cost: {(end - start):.2f}\n')
     log.close()
-    np.save(f'shift_{i_task}.npy', shift)
+    np.save(f'{temp_folder}/shift_{i_task}.npy', shift)
     print(f'task {i_task} costs {(end - start):.2f} seconds')
     return first, last
 
 
-def shift_interior_points(input: str, rbf_solutions_npy: str, bc_points: np.ndarray,
-        k_neighbor: int, radius: float, verbose: bool):
-    if verbose:
-        print('loading the CGNS tree ...')
-    cgns, zone, zone_size = wrapper.getUniqueZone(input)
+def shift_interior_points(rbf_folder: str, bc_points: np.ndarray, args: str):
+    print('\nloading the volume mesh ...')
+    cgns, zone, zone_size = wrapper.getUniqueZone(args.mesh)
     global global_x, global_y, global_z
     _, global_x, global_y, global_z = wrapper.readPoints(zone, zone_size)
-    print(np.linalg.norm(global_x), np.linalg.norm(global_y), np.linalg.norm(global_z))
     n_point = zone_size[0][0]
+    n_cell = zone_size[0][1]
     assert n_point == len(global_x) == len(global_y) == len(global_z)
 
-    if verbose:
-        print('loading the RBF solution ...')
-    rbf_solutions = np.load(rbf_solutions_npy)
+    print('loading the RBF solution ...')
+    rbf_solutions = np.load(f'{rbf_folder}/rbf_solutions.npy')
     global u_rbf, v_rbf, w_rbf
     u_rbf = np.array(rbf_solutions[:, X])
     v_rbf = np.array(rbf_solutions[:, Y])
     w_rbf = np.array(rbf_solutions[:, Z])
 
-    if verbose:
-        print('building the KDTree ...')
+    print('building the KDTree ...')
     global kdtree
     kdtree = KDTree(bc_points)
 
-    executor = ProcessPoolExecutor(max_workers=16)
-    n_task = 1024
+    print('shifting interior points by futures ...')
+    temp_folder = f'{rbf_folder}/temp{np.random.randint(0, 2**32)}'
+    os.makedirs(temp_folder, exist_ok=True)
+    executor = ProcessPoolExecutor(args.n_worker)
+    n_task = args.n_task
     tasks = []
     for i_task in range(n_task):
         tasks.append(executor.submit(
-            shift_points_by_futures_process, i_task, n_task, k_neighbor, radius))
+            shift_points_by_futures_process, i_task, n_task, temp_folder, args))
     done, not_done = wait(tasks)
-    print('done:')
-    for x in done:
-        print(x)
-    print('not_done:')
-    for x in not_done:
-        print(x)
+    assert len(not_done) == 0, not_done
     # write back the coords
     global_shift_x = np.ndarray((n_point,), global_x.dtype)
     global_shift_y = np.ndarray((n_point,), global_x.dtype)
@@ -271,23 +233,14 @@ def shift_interior_points(input: str, rbf_solutions_npy: str, bc_points: np.ndar
     for i_task in range(n_task):
         first, last = tasks[i_task].result()
         print(i_task, first, last)
-        local_shift = np.load(f'shift_{i_task}.npy')
+        local_shift = np.load(f'{temp_folder}/shift_{i_task}.npy')
         global_shift_x[first : last] = local_shift[:, X]
         global_shift_y[first : last] = local_shift[:, Y]
         global_shift_z[first : last] = local_shift[:, Z]
+    shutil.rmtree(temp_folder)
     global_x += global_shift_x
     global_y += global_shift_y
     global_z += global_shift_z
-    print(np.linalg.norm(global_x), np.linalg.norm(global_y), np.linalg.norm(global_z))
-    if k_neighbor > 0:
-        assert radius == 0.0
-        suffix = f'k={k_neighbor}'
-    elif radius > 0:
-        assert k_neighbor == 0
-        suffix = f'r={radius:.2e}'
-    output = f'shifted_{input[:-5]}_{suffix}.cgns'
-    if verbose:
-        print(f'writing to {output} ... ')
 
     # write the shifts as a vector field of point data
     point_data = wrapper.getChildrenByType(zone, 'FlowSolution_t')
@@ -297,6 +250,9 @@ def shift_interior_points(input: str, rbf_solutions_npy: str, bc_points: np.ndar
     cgl.newDataArray(point_data, 'ShiftX', global_shift_x)
     cgl.newDataArray(point_data, 'ShiftY', global_shift_y)
     cgl.newDataArray(point_data, 'ShiftZ', global_shift_z)
+
+    output = f'{rbf_folder}/shifted_{n_cell}cells_{n_point}points.cgns'
+    print(f'writing to {output} ... ')
     cgm.save(output, cgns)
 
 
@@ -307,10 +263,12 @@ if __name__ == '__main__':
     parser.add_argument('--mesh', type=str, help='the CGNS file of the mesh to be shifted')
     parser.add_argument('--old_points', type=str, help='the NPY file of boundary points before shifting')
     parser.add_argument('--new_points', type=str, help='the NPY file of boundary points after shifting')
+    parser.add_argument('--folder', type=str, help='the folder containing new_points and output')
+    parser.add_argument('--n_worker',  type=int, help='number of workers for running futures')
+    parser.add_argument('--n_task',  type=int, help='number of tasks for running futures, usually >> n_worker')
     parser.add_argument('--k_neighbor', type=int, default=0, help='number of neighbors in the support of the RBF basis')
     parser.add_argument('--radius', type=float, default=0.0, help='the radius of the RBF basis')
-    parser.add_argument('--output', type=str, help='the output mesh file')
-    parser.add_argument('--verbose', default=True, action='store_true')
+    parser.add_argument('--verbose', default=False, action='store_true')
     args = parser.parse_args()
 
     x = np.linspace(0, 1, 101)
@@ -326,12 +284,18 @@ if __name__ == '__main__':
     plt.savefig('RBF.svg')
 
     old_points = np.load(args.old_points)
-    new_points = np.load(args.new_points)
+    new_points = np.load(f'{args.folder}/{args.new_points}')
     assert old_points.shape == new_points.shape
 
-    rbf_matrix_npz = build_rbf_matrix(old_points, args.k_neighbor, args.radius, args.verbose)
-    rbf_solutions_npy = solve_rbf_system(rbf_matrix_npz, new_points - old_points, args.verbose)
-    shift_interior_points(args.mesh, rbf_solutions_npy, old_points, args.k_neighbor, args.radius, args.verbose)
+    if args.k_neighbor > 0:
+        assert args.radius == 0.0
+        rbf_folder = f'{args.folder}/k={args.k_neighbor}'
+    elif args.radius > 0:
+        assert args.k_neighbor == 0
+        rbf_folder = f'{args.folder}/r={args.radius:.2e}'
 
-    # n_point = int(sys.argv[1])
-    # test_on_random_points(n_point, radius=0.1, verbose=True)
+    os.makedirs(rbf_folder, exist_ok=True)
+
+    build_rbf_matrix(rbf_folder, old_points)
+    solve_rbf_system(rbf_folder, new_points - old_points)
+    shift_interior_points(rbf_folder, old_points, args)
