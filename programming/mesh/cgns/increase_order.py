ed1ea143bad68c99579e14460211c14e101e06cf
Weicheng <weicheng.pei@icloud.com>
2024-11-13 13:16:24 +0800

    Support converting `HEXA_8` to `HEXA_27`.

--- programming/mesh/cgns/increase_order.py
+++ programming/mesh/cgns/increase_order.py
@@ -5,20 +5,82 @@ import CGNS.PAT.cgnskeywords as cgk
 import wrapper
 
 import numpy as np
-from scipy.spatial import KDTree
 import argparse
 import sys
+import os
 
 
 X, Y, Z = 0, 1, 2
 
 
+type_to_local_coords = dict()
+type_to_local_coords['QUAD_4'] = np.array([
+    [-1, -1], [+1, -1], [+1, +1], [-1, +1]
+], dtype=float)
+type_to_local_coords['QUAD_9'] = np.array([
+    [-1, -1], [+1, -1], [+1, +1], [-1, +1],
+    [0, -1], [+1, 0], [0, +1], [-1, 0], [0, 0]
+], dtype=float)
+type_to_local_coords['HEXA_8'] = np.array([
+    [-1, -1, -1], [+1, -1, -1], [+1, +1, -1], [-1, +1, -1],
+    [-1, -1, +1], [+1, -1, +1], [+1, +1, +1], [-1, +1, +1]
+], dtype=float)
+type_to_local_coords['HEXA_27'] = np.array([
+    [-1, -1, -1], [+1, -1, -1], [+1, +1, -1], [-1, +1, -1],
+    [-1, -1, +1], [+1, -1, +1], [+1, +1, +1], [-1, +1, +1],
+    # mid-edge points:
+    [0, -1, -1], [+1, 0, -1], [0, +1, -1], [-1, 0, -1],
+    [-1, -1, 0], [+1, -1, 0], [+1, +1, 0], [-1, +1, 0],
+    [0, -1, +1], [+1, 0, +1], [0, +1, +1], [-1, 0, +1],
+    # mid-face points:
+    [0, 0, -1],
+    [0, -1, 0], [+1, 0, 0], [0, +1, 0], [-1, 0, 0],
+    [0, 0, +1],
+    # mid-cell point:
+    [0, 0, 0]
+], dtype=float)
+
+
+type_to_shape_functions = dict()
+type_to_shape_functions['QUAD_4'] = lambda local_fixed, local_query: \
+    (1 + local_query[X] * local_fixed[:, X]) * \
+    (1 + local_query[Y] * local_fixed[:, Y]) / 4.0
+type_to_shape_functions['HEXA_8'] = lambda local_fixed, local_query: \
+    (1 + local_query[X] * local_fixed[:, X]) * \
+    (1 + local_query[Y] * local_fixed[:, Y]) * \
+    (1 + local_query[Z] * local_fixed[:, Z]) / 8.0
+
+
+def get_type_info(type_val: int) -> tuple[str, int]:
+    return cgk.ElementType_l[type_val], cgk.ElementTypeNPE_l[type_val]
+
+
+def get_new_type_info(old_type_val: int, order: int) -> tuple[str, int]:
+    # TODO(PVC): support higher orders
+    old_type_str, old_type_npe = get_type_info(old_type_val)
+    new_type_str = str()
+    if old_type_str == 'QUAD_4':
+        if order == 2:
+            new_type_str = 'QUAD_9'
+        else:
+            assert False, (old_type_str, order)
+    elif old_type_str == 'HEXA_8':
+        if order == 2:
+            new_type_str = 'HEXA_27'
+        else:
+            assert False, (old_type_str, order)
+    else:
+        assert False, (old_type_str, order)
+    new_type_npe = int(new_type_str[new_type_str.find('_') + 1:])
+    return new_type_str, new_type_npe
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog = f'python3 {sys.argv[0]}',
-        description='Increase the order of geometric approximation.')
+        description='Increase the order of elements without merging new points.')
+    parser.add_argument('--folder', type=str, help='the working directory')
     parser.add_argument('--input', type=str, help='the input linear mesh file')
-    parser.add_argument('--output', type=str, help='the output high-order mesh file')
     parser.add_argument('--order', type=int, default=2, help='order of the output mesh')
     parser.add_argument('--verbose', default=False, action='store_true')
     args = parser.parse_args()
@@ -26,14 +88,14 @@ if __name__ == "__main__":
     print(args)
 
     # load the linear mesh
-    mesh_cgns, _, _ = cgm.load(args.input)
+    mesh_cgns, _, _ = cgm.load(f'{args.folder}/{args.input}')
 
     mesh_zone = wrapper.getUniqueChildByType(
         wrapper.getUniqueChildByType(mesh_cgns, 'CGNSBase_t'), 'Zone_t')
     mesh_zone_size = wrapper.getNodeData(mesh_zone)
-    n_node = mesh_zone_size[0][0]
+    n_node_old = mesh_zone_size[0][0]
     n_cell = mesh_zone_size[0][1]
-    print(f'in linear mesh: n_node = {n_node}, n_cell = {n_cell}')
+    print(f'before converting: n_node = {n_node_old}, n_cell = {n_cell}')
 
     mesh_coords = wrapper.getChildrenByType(
         wrapper.getUniqueChildByType(mesh_zone, 'GridCoordinates_t'), 'DataArray_t')
@@ -41,90 +103,62 @@ if __name__ == "__main__":
 
     section = wrapper.getUniqueChildByType(mesh_zone, 'Elements_t')
     element_type = wrapper.getNodeData(section)
-    assert element_type[0] == 7  # QUAD_4
+
+    # get old and new type info
+    old_type_val = element_type[0]
+    old_type_str, old_type_npe = get_type_info(old_type_val)
+    new_type_str, new_type_npe = get_new_type_info(old_type_val, args.order)
+    print(f'converting ({old_type_str}, {old_type_npe}) -> ({new_type_str}, {new_type_npe})')
+    old_local_coords = type_to_local_coords[old_type_str]
+    assert old_type_npe == len(old_local_coords)
+    new_local_coords = type_to_local_coords[new_type_str]
+    assert new_type_npe == len(new_local_coords)
+    old_shape_function = type_to_shape_functions[old_type_str]
+
     old_connectivity = wrapper.getNodeData(
         wrapper.getUniqueChildByName(section, 'ElementConnectivity'))
 
-    # add center and mid-edge points:
-    n_node_new = n_node + n_cell * 5  # TODO(PVC): support higher orders
-    new_coords_x = np.ndarray((n_node_new,), dtype=mesh_coords_x.dtype)
-    new_coords_x[0 : n_node] = mesh_coords_x
-    new_coords_y = np.ndarray((n_node_new,), dtype=mesh_coords_y.dtype)
-    new_coords_y[0 : n_node] = mesh_coords_y
-    new_coords_z = np.ndarray((n_node_new,), dtype=mesh_coords_z.dtype)
-    new_coords_z[0 : n_node] = mesh_coords_z
-    i_node_next = n_node
-    new_connectivity = np.ndarray((old_connectivity.shape[0] * 9 // 4,),
-        dtype=old_connectivity.dtype)
-    for i_cell_mesh in range(n_cell):
-        old_first = i_cell_mesh * 4
-        new_fisrt = i_cell_mesh * 9
-        new_connectivity[new_fisrt : new_fisrt + 4] = old_connectivity[old_first : old_first + 4]
-        # build the mid-point on the bottom edge
-        x = (mesh_coords_x[old_first + 0] + mesh_coords_x[old_first + 1]) / 2
-        y = (mesh_coords_y[old_first + 0] + mesh_coords_y[old_first + 1]) / 2
-        z = (mesh_coords_z[old_first + 0] + mesh_coords_z[old_first + 1]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
-        i_node_next += 1
-        new_connectivity[new_fisrt + 4] = i_node_next
-        # build the mid-point on the right edge
-        x = (mesh_coords_x[old_first + 1] + mesh_coords_x[old_first + 2]) / 2
-        y = (mesh_coords_y[old_first + 1] + mesh_coords_y[old_first + 2]) / 2
-        z = (mesh_coords_z[old_first + 1] + mesh_coords_z[old_first + 2]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
-        i_node_next += 1
-        new_connectivity[new_fisrt + 5] = i_node_next
-        # build the mid-point on the top edge
-        x = (mesh_coords_x[old_first + 2] + mesh_coords_x[old_first + 3]) / 2
-        y = (mesh_coords_y[old_first + 2] + mesh_coords_y[old_first + 3]) / 2
-        z = (mesh_coords_z[old_first + 2] + mesh_coords_z[old_first + 3]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
-        i_node_next += 1
-        new_connectivity[new_fisrt + 6] = i_node_next
-        # build the mid-point on the left edge
-        x = (mesh_coords_x[old_first + 3] + mesh_coords_x[old_first + 0]) / 2
-        y = (mesh_coords_y[old_first + 3] + mesh_coords_y[old_first + 0]) / 2
-        z = (mesh_coords_z[old_first + 3] + mesh_coords_z[old_first + 0]) / 2
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
-        i_node_next += 1
-        new_connectivity[new_fisrt + 7] = i_node_next
-        # build the center point
-        index = old_connectivity[old_first : old_first + 4] - 1
-        x = np.sum(mesh_coords_x[index]) / 4
-        y = np.sum(mesh_coords_y[index]) / 4
-        z = np.sum(mesh_coords_z[index]) / 4
-        new_coords_x[i_node_next] = x
-        new_coords_y[i_node_next] = y
-        new_coords_z[i_node_next] = z
-        i_node_next += 1
-        new_connectivity[new_fisrt + 8] = i_node_next
+    # add new points and update connectivity:
+    n_node_new = n_node_old + n_cell * (new_type_npe - old_type_npe)
+    new_coords_x = np.resize(mesh_coords_x, n_node_new)
+    new_coords_y = np.resize(mesh_coords_y, n_node_new)
+    new_coords_z = np.resize(mesh_coords_z, n_node_new)
+    i_node_next = n_node_old
+    assert old_connectivity.shape == (n_cell * old_type_npe,)
+    new_connectivity = np.resize(old_connectivity, n_cell * new_type_npe)
+    for i_cell in range(n_cell):
+        old_first = i_cell * old_type_npe
+        new_fisrt = i_cell * new_type_npe
+        # copy original connectivity
+        old_index = old_connectivity[old_first : old_first + old_type_npe]
+        new_connectivity[new_fisrt : new_fisrt + old_type_npe] = old_index
+        for i_local in range(old_type_npe, new_type_npe):
+            shape = old_shape_function(old_local_coords, new_local_coords[i_local])
+            new_coords_x[i_node_next] = np.dot(mesh_coords_x[old_index - 1], shape)
+            new_coords_y[i_node_next] = np.dot(mesh_coords_y[old_index - 1], shape)
+            new_coords_z[i_node_next] = np.dot(mesh_coords_z[old_index - 1], shape)
+            i_node_next += 1
+            new_connectivity[new_fisrt + i_local] = i_node_next
         if args.verbose:
-            print(f'QUAD_4[{i_cell_mesh}] converted')
+            print(f'cell[{i_cell} / {n_cell}] converted')
     assert i_node_next == n_node_new
-    n_node = n_node_new
-    print(f'in quadratic mesh: n_node = {n_node}, n_cell = {n_cell}')
+    print(f'after converting: n_node = {n_node_new}, n_cell = {n_cell}')
 
     # update the cells
     cgu.removeChildByName(section, 'ElementConnectivity')
     cgl.newDataArray(section, 'ElementConnectivity', new_connectivity)
 
     # write the new mesh out
-    mesh_zone_size[0][0] = n_node
-    element_type[0] = 9  # QUAD_9
+    mesh_zone_size[0][0] = n_node_new
+    new_type_val = cgk.ElementType_l.index(new_type_str)
+    assert get_type_info(new_type_val)[0] == new_type_str
+    element_type[0] = new_type_val
     mesh_coords[X][1] = new_coords_x
     mesh_coords[Y][1] = new_coords_y
     mesh_coords[Z][1] = new_coords_z
 
-    output = args.output
-    if output is None:
-        output = f'order={args.order}_{args.input}'
+    output_folder = f'{args.folder}/order={args.order}'
+    os.makedirs(output_folder, exist_ok=True)
+    output = f'{output_folder}/to_be_merged.cgns'
     print('writing to', output)
     cgm.save(output, mesh_cgns)
