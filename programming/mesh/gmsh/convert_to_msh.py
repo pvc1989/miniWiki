24d02e1731ca7cd9df70734cc8a6b8def42b4648
Weicheng <weicheng.pei@icloud.com>
2024-11-29 14:26:11 +0800

    Print elementary entities and physical groups.

--- programming/mesh/gmsh/convert_to_msh.py
+++ programming/mesh/gmsh/convert_to_msh.py
@@ -4,6 +4,43 @@ import sys
 import gmsh
 
 
+def print_physical_groups() -> dict:
+    print()
+    entity_to_group = dict()
+    groups = gmsh.model.get_physical_groups()
+    for group in groups:
+        dim, tag = group
+        name = gmsh.model.get_physical_name(dim, tag)
+        print(f'{dim}d Physical Group {group} "{name}"')
+        tags = gmsh.model.get_entities_for_physical_group(dim, tag)
+        for tag in tags:
+            entity = (dim, int(tag))
+            entity_to_group[entity] = group
+            name = gmsh.model.get_entity_name(dim, tag)
+            print(f'  {dim}d Elementary Entity {entity} "{name}"')
+    return entity_to_group
+
+
+def print_elementary_entities(entity_to_group: dict):
+    print()
+    entities = gmsh.model.get_entities()
+    for entity in entities:
+        dim, tag = entity
+        name = gmsh.model.get_entity_name(dim, tag)
+        # group = gmsh.model.get_physical_groups_for_entity(dim, tag)
+        print(f'{dim}d Elementary Entity {entity} "{name}"')
+        if entity in entity_to_group:
+            group = entity_to_group[entity]
+            dim, tag = group
+            name = gmsh.model.get_physical_name(dim, tag)
+            print(f'  {dim}d Physical Group {group} "{name}" found')
+        else:
+            tag = gmsh.model.add_physical_group(dim, [tag], tag, str(entity))
+            name = gmsh.model.get_physical_name(dim, tag)
+            group = (dim, tag)
+            print(f'  {dim}d Physical Group {group} "{name}" created')
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog = f'python3 {sys.argv[0]}',
@@ -21,6 +58,10 @@ if __name__ == "__main__":
 
     input = f'{args.folder}/{args.mesh}'
     gmsh.merge(input)
+
+    entity_to_group = print_physical_groups()
+    print_elementary_entities(entity_to_group)
+    entity_to_group = print_physical_groups()
 
     dot_pos = input.rfind('.')
     output = f'{input[:dot_pos]}.msh'
